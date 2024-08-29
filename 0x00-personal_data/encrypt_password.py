b2f0bec968bf17387417e2adc51f21e4f20d0b9d
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-29 19:08:06 +0300

    implement password hashing

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""module: encrypt_password
+Implement a hash_password function that expects one string
+argument name password and returns a salted, hashed password,
+which is a byte string.
+
+Use the bcrypt package to perform the hashing (with hashpw).
+"""
+import bcrypt
+import base64
+import hashlib
+
+def hash_password(password: str):
+    """
+    returns a salted, hashed password, which is a byte string.
+    Args:
+        password (str) -> password
+    Returns
+        hashed password in bytes
+    """
+    return bcrypt.hashpw(
+            base64.b64encode(hashlib.sha256(password.encode()).digest()),
+            bcrypt.gensalt()
+            )
