027fa7b221c45e4494a40138df233786a600ce8e
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-29 16:46:53 +0300

    handle pep coding standards

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,18 +2,23 @@
 """module: filtered_logger
 """
 # Write a function called filter_datum that returns the log message obfuscated:
-# 
+#
 #     Arguments:
 #         fields: a list of strings representing all fields to obfuscate
 #         redaction: a string representing by what the field will be obfuscated
 #         message: a string representing the log line
-#         separator: a string representing by which character is separating all fields in the log line (message)
-#         The function should use a regex to replace occurrences of certain field values.
-#         filter_datum should be less than 5 lines long and use re.sub to perform the substitution with a single regex.
+#         separator: a string representing by which character is separating all
+#            fields in the log line (message)
+#         The function should use a regex to replace occurrences of certain
+#            field values.
+#         filter_datum should be less than 5 lines long and
+#            use re.sub to perform
+#            the substitution with a single regex.
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List, redaction: str, message: str, separator: str):
     """filters PII on ley fields using regex
     Args:
         fields (list) -> sensitive keys of data
@@ -23,5 +28,10 @@ def filter_datum(fields, redaction, message, separator):
     Returns
         list of sanitised entries
     """
-    pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
-            return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
+    pattern = '|'.join(
+            [f'{field}=[^{separator}]*'
+                for field in fields])
+    return re.sub(
+            pattern,
+            lambda m: f"{m.group(0).split('=')[0]}={redaction}",
+            message)
