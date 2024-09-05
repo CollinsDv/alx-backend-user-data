3f078008a79791c180db1445349bde494ab4e475
Collins Kimani <mwas9446.ck@gmail.com>
2024-09-05 03:54:08 +0300

    implement create_session method for in-memory session storage

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,25 @@
 """session auth class
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """the session class implementation
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id
+        Args:
+            user_id
+        Returns:
+            uuid string
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        user_id_by_session_id[session_id] = user_id
+
+        return session_id
