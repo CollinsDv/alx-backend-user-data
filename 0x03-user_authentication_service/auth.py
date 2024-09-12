5ade5ba1058e42a64f866229840c55f305c61c2e
Collins Kimani <mwas9446.ck@gmail.com>
2024-09-13 02:58:04 +0300

    add get_user_from_session_id for user recovery

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,20 @@ class Auth:
             return session_id
 
         return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """returns a user based on a session_id
+        Args:
+            session_id: session id for obtaining user
+        Returns:
+            User object or None
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
+        else:
+            return user
