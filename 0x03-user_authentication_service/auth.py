c89850f42c863b7258a5dd8e87758984690bfac7
Collins Kimani <mwas9446.ck@gmail.com>
2024-09-12 04:22:09 +0300

    add registration mechanism

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 """
 import bcrypt
 import hashlib
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,3 +18,27 @@ def _hash_password(password: str) -> bytes:
             hashlib.sha256(password.encode()).digest(),
             bcrypt.gensalt()
             )
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers user
+        Args
+            email: user email
+            password: user credentials
+        Returns:
+            user object or raise valueerror
+        """
+        # check if user exists
+        if self._db._session.query(User).filter_by(email=email).first():
+            raise ValueError(f"User {email} already exists")
+
+        hashed_pwd = _hash_password(password)
+
+        return self._db.add_user(email, hashed_pwd)
