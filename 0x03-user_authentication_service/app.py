77556293417909af2a00d37e8267d90dc3b15724
Collins Kimani <mwas9446.ck@gmail.com>
2024-09-13 04:14:31 +0300

    add view for obtaining user details

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,8 +58,21 @@ def logout():
         abort(403)
 
     AUTH.destroy_session(user.id)
-    return redirect(url_for('/'))
+    return redirect(url_for('index'))
 
+
+@app.route('/profile', strict_slashes=False)
+def profile():
+    """gets a user profile
+    """
+    sesh_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(sesh_id)
+
+    if not user:
+        abort(403)
+
+    return jsonify({"email": user.email})
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
